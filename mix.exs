de23c8a58da1f9e412f6a72510aabacca5b9c2a0
Grant Murphy <grant.murphy@elastic.co>
2019-04-23 16:52:04 +1000

    fix(pkg) rename packaged app to metasploit

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Metasploit.MixProject do
 
   def project do
     [
-      app: :metasploit_ex,
+      app: :metasploit,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
